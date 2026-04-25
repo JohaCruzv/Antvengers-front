3e15eb7f808d9db105316d83e16240f196e004eb
JohaCruzv <johana.cruzvillalba@gmail.com>
2026-04-24 19:19:26 -0500

    definicion de endpoints y URL base de la api

---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+const URL_BASE = "localhost:8080/"
+
+export let endpoints = {
+  login: URL_BASE + "login/",
+  register: URL_BASE + "register/",
+  gastos: URL_BASE + "gastos/",
+  categorias: URL_BASE + "categorias/",
+};
